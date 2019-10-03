5c466f66ee2f176fdf9a347fed773448a3ff06be
Jessica Strait <48388653+jesslynne73@users.noreply.github.com>
2019-10-02 22:28:34 -0400

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,31 @@
+# We are creating a BMI calculator for the user, upon the input of their height and weight.
+# The program will then tell the user if they are overweight, underweight, or optimal weight
+# based on the calculated BMI.
+# Author: Jessica Strait, CMPSC 131 Sec 001
+
+# First, the program will prompt the user to enter their weight.
+
+weight = float(input("Enter your weight in pounds."))
+
+# Next, the program will prompt the user to enter their heights.
+
+height = float(input("Enter your height in inches."))
+
+# We will assign the variable "BMI" to calculate our target using the two earlier variables of
+# weight and height as entered by the user.
+
+BMI = weight*703/height**2
+print("Your BMI is", BMI)
+
+# Now, we will tell the program how to determine if a BMI is overweight, underweight, or
+# optimal weight based on the calculated number.
+
+underweight = BMI <= 18.5
+if underweight:
+    print("Your BMI indicates that you are underweight.")
+else:
+    overweight = BMI >= 25
+    if overweight:
+        print("Your BMI indicates that you are overweight.")
+    else:
+        print("Your BMI indicates that you are optimal weight.")
