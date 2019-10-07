609ff8004a599bb513d8e9ee659a400c308999a2
Jessica Strait <48388653+jesslynne73@users.noreply.github.com>
2019-10-07 13:02:06 -0400

    Update Intro-To-Sets.py

--- Intro-To-Sets.py
+++ Intro-To-Sets.py
@@ -1,7 +1,10 @@
 # Author: Jessica Strait
 # This is an exercise in the set data type: an unordered group of elements in which no element repeats.
-# This program will take a list of integers, create a set of the distinct values, and average all distinct elements.
+# The first program will take a list of integers, create a set of the distinct values, and average all distinct elements.
+# The second program will take a set quantity of stamps and their countries, and use the add operator to determine how many countries we have stamps from.
 
+
+### Average Numbers begins here.
 def average(array):
     # Instantiate an empty set and the Boolean variable we will use.
     set = []
@@ -31,3 +34,27 @@ n = int(input())
 arr = list(map(int, input().split()))
 result = average(arr)
 print(result)
+
+
+### Stamp Collector begins here.
+# Instantiate a set
+s = set()
+# Take a fixed quantity as input and initialize variables.
+stamp_quantity = int(input())
+equivalence = False
+# For each new stamp, check if we have already counted the country or not yet. If not, add it to the set.
+for _ in range(stamp_quantity):
+    country = input()
+    for value in s:
+        if country != value:
+            equivalence = False
+        else:
+            equivalence = True
+            break
+    if equivalence is True:
+        pass
+    else:
+        s.add(country)
+# Return how many unique countries we have stamps from.
+print(len(s))
+
